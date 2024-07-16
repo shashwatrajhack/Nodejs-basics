0da034313bf654afb6bedacee73b6a8bf3808644
shashwatrajhack <er.shashwatraj@gmail.com>
2024-07-16 12:11:54 +0530

    node modules basic

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const path = require("path"); //now this become object with bunch of useful method
-const os = require("os"); //this module information about operating system
+//const path = require("path"); //now this become object with bunch of useful method
+//const os = require("os"); //this module information about operating system
 const EventEmitter = require('events');//this represents the eventemitter is a class not a function.
-const freeMemory = os.freemem();
-const totalMemory = os.totalmem();
-const osObj = os.uptime();
-console.log(osObj);
-console.log(`Total Memory ${totalMemory}`);
-console.log(`Free Memory ${freeMemory}`);
+const Logger = require('./logger');
+//const freeMemory = os.freemem();
+//const totalMemory = os.totalmem();
+//const osObj = os.uptime();
+//console.log(osObj);
+//console.log(`Total Memory ${totalMemory}`);
+//console.log(`Free Memory ${freeMemory}`);
 //const pathObj = path.parse(__filename);
 
 //console.log(pathObj);
@@ -17,20 +18,27 @@ console.log(`Free Memory ${freeMemory}`);
 
 //class is a container for properties and functions which we call method
 
-const emitter = new EventEmitter();
+
 
 // A listener is a function that will be called when an event is raised
 //register a listener
-
-emitter.on('messageLogged',function(arg){
-    console.log("listener called",arg);
+const logger = new Logger();
+logger.on('messageLogged',(arg) => {
+    console.log("Message is logged");
 });
+
+logger.log('message')
+
+
+// emitter.on('messageLogged',function(arg){
+//     console.log("listener called",arg);
+// });
 
 
 //raise an event
 //event with arguments called event-arguments
 
 
-emitter.emit('messageLogged',{id:1, url:'http//'});
+//emitter.emit('messageLogged',{id:1, url:'http//'});
 
 
--- logger.js
+++ logger.js
@@ -1,9 +1,18 @@
-var url = 'http://mylogger.io/log';
+const EventEmitter = require('events');
 
-function log(message){
-    //send http request
-    console.log("Message");
+
+var url = 'http://mylogger.io/log';
+class Logger extends EventEmitter{
+    log(message){
+        //send http request
+        console.log("Message");
+
+        //raise an event
+        this.emit('messageLogged',{id:1,url:'http://'});
+    
+    }
 
 }
 
-module.exports.log = log;
+
+module.exports = Logger;
